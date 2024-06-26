a2cb984823185e8dfdda3806ef7aff7b3e37bafe
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-06-26 18:39:14 +0530

    2nd commit

---
+++ vaiables.dart
@@ -0,0 +1,11 @@
+void main(){
+  //var variable depend on vaiable type.
+  var name = "gyanendra";
+  var address =  "xyz";
+  var phone_no = "1234567890";
+  print("name=" +name);
+   print("Address="+address);
+   print("phone_no="+phone_no);
+   
+   
+}
