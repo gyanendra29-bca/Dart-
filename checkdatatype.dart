db9d6446ba29a12a4a7a3a77bb2737818bb30cbc
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-06-27 16:24:29 +0530

    4th commit

---
+++ checkdatatype.dart
@@ -0,0 +1,28 @@
+
+  void main(){
+  int cart_item = 10;
+   print(cart_item);
+
+   double price = 10.50;
+   print(price);
+
+   String city = "Ranchi";
+   print(city);
+
+   bool is_completed = true;
+   print(is_completed);
+
+   bool is_login = false;
+   print(is_login);
+
+   var quantity = 100;
+   print(quantity);
+
+   print(cart_item.runtimeType);
+ print(price.runtimeType);
+ print(city.runtimeType);
+ print(is_completed.runtimeType);
+ print(is_login.runtimeType);
+ print(quantity.runtimeType);
+ 
+}
