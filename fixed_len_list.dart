c51ea78f4baa69fca465642b988dd8b31b91b11b
gyanendra29-bca <gyanendratiwari200@gmail.com>
2024-06-30 14:31:48 +0530

    16th commit

---
+++ fixed_len_list.dart
@@ -0,0 +1,11 @@
+void main(){
+   var lst = new List(3);
+   lst[0] = "Sonam";
+   lst[1] = "Jay";
+   lst[2] = 100;
+   print(lst);
+   print(lst[0]);
+   print(lst[1]);
+   print(lst[2]);
+   print(lst.runtimeType);
+}
